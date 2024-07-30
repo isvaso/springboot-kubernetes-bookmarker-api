0edb27cb6b445dca463725891359dbf1bbb2d384
Ilya Biryukov <is.vaso@gmail.com>
2024-07-30 17:21:35 +0600

    Create and implement BookmarksDTO

--- bookmarker-api/src/main/java/com/isvaso/bookmarkerapi/domain/BookmarksService.java
+++ bookmarker-api/src/main/java/com/isvaso/bookmarkerapi/domain/BookmarksService.java
@@ -7,8 +7,6 @@ import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 @Transactional
 @RequiredArgsConstructor
@@ -17,9 +15,9 @@ public class BookmarksService {
     private final BookmarkRepository repository;
 
     @Transactional(readOnly = true)
-    public List<Bookmark> getBookmarks(Integer page) {
+    public BookmarksDTO getBookmarks(Integer page) {
         int pageNumber = page < 1 ? 0 : page - 1;
         Pageable pageable = PageRequest.of(pageNumber, 10, Sort.Direction.DESC, "createdAt");
-        return repository.findAll(pageable).getContent();
+        return new BookmarksDTO(repository.findAll(pageable));
     }
 }
