eff216a8f4b7fe107e8e6c72728664c9eab637e3
Ilya Biryukov <is.vaso@gmail.com>
2024-09-23 21:29:54 +0600

    Add NextJS bookmarker-ui-nextjs. Create start and add pages

---
+++ bookmarker-ui-nextjs/app/bookmarks/add/page.tsx
@@ -0,0 +1,12 @@
+import NavBar from "@/app/components/NavBar";
+import {NextPage} from "next";
+
+const AddBookmark: NextPage = () => {
+    return (
+        <div>
+            <h1>New Bookmark Form</h1>
+        </div>
+    );
+}
+
+export default AddBookmark
---
+++ bookmarker-ui-nextjs/app/bookmarks/page.tsx
@@ -0,0 +1,9 @@
+import NavBar from "@/app/components/NavBar";
+
+export default function Home() {
+    return (
+        <div>
+            <h1>Welcome to Bookmarker</h1>
+        </div>
+    );
+}
