02714dfebe51aebcc055dd015a89c529bd97195f
Ilya Biryukov <is.vaso@gmail.com>
2024-09-29 02:12:49 +0600

    Add UI test, but with nothing and disable test in workflow

--- bookmarker-ui-nextjs/app/components/__tests__/Bookmark.test.tsx
+++ bookmarker-ui-nextjs/app/components/__tests__/Bookmark.test.tsx
@@ -1,17 +1,28 @@
-// __tests__/Bookmark.test.tsx
-import React from 'react';
-import '@testing-library/jest-dom';
-
-import Bookmark from '../Bookmark';
-import { Bookmark as BookmarkModel } from '../../services/models';
-import expect from "expect";
-
-const mockBookmark: BookmarkModel = {
-  id: 1,
-  title: 'Test Bookmark',
-  url: 'https://example.com',
-  created_at: new Date('2024-09-29'),
-  updated_at: new Date('2024-09-29')
-};
-
-
+// // __tests__/Bookmark.test.tsx
+// import React from 'react';
+// import { render, screen } from '@testing-library/react';
+// import '@testing-library/jest-dom';
+//
+// import Bookmark from '../Bookmark';
+// import { Bookmark as BookmarkModel } from '../../services/models';
+//
+// const mockBookmark: BookmarkModel = {
+//   id: 1,
+//   title: 'Test Bookmark',
+//   url: 'https://example.com',
+//   created_at: new Date('2024-09-29'),
+//   updated_at: new Date('2024-09-29')
+// };
+//
+// test('renders bookmark component with correct title and link', () => {
+//   render(<Bookmark bookmark={mockBookmark} />);
+//
+//   // Проверка наличия заголовка
+//   const titleElement = screen.getByText(/Test Bookmark/i);
+//   expect(titleElement).toBeInTheDocument();
+//
+//   // Проверка ссылки с правильным URL и атрибутом target
+//   const linkElement = screen.getByRole('link', { name: /Test Bookmark/i });
+//   expect(linkElement).toHaveAttribute('href', 'https://example.com');
+//   expect(linkElement).toHaveAttribute('target', '_blank');
+// });
